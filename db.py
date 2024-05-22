392f28a89dbb9ed0d3de540b71a6d24072a520dd
amralt <amralt88@gmail.com>
2024-05-22 21:25:42 +0600

    записаь в бд

--- db.py
+++ db.py
@@ -8,13 +8,14 @@ class ProblemsDataBase():
     def __init__(self) -> None:
         self.connect = sqlite3.connect('problems.db')
         self.cursor = self.connect.cursor()
+        self.create_tables()
 
     def create_tables(self):
         cursor = self.cursor
         # структура задач
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS Problems (
-            problemId INTEGER PRIMARY KEY,
+            problemId TEXT PRIMARY KEY, 
             problem_index CHAR(1) NOT NULL,
             name TEXT NOT NULL,
             type TEXT NOT NULL,
@@ -41,34 +42,45 @@ class ProblemsDataBase():
         PRIMARY KEY (tagId, problemId)
         )
         """)
-        
+        self.connect.commit()
+
+        # TODO: проверка что таблица существует 
+        for pair in tagsId.items():
+            cursor.execute(f"INSERT INTO Tags (tagId, tagName) VALUES ({pair[0]}, '{pair[1]}' )")
+
 
     def get_problems_by_tag(self, tag_id):
         cursor = self.cursor
-        cursor.execute("SELECT p.problemId, p.problem_index, p.name, p.type, p.points, p.rating, p.statistic FROM Problems p JOIN Problems_Tags pt ON p.problemId = pt.problemId WHERE pt.tagId =?", (tag_id,))
+        cursor.execute("SELECT * FROM Problems p JOIN Problems_Tags pt ON p.problemId = pt.problemId WHERE pt.tagId =?", (tag_id,))
         rows = cursor.fetchall()
         return rows
+    
+    def get_problem_by_id(self, problem_id):
+        cursor = self.cursor
+        cursor.execute("SELECT * FROM Problems WHERE problemId LIKE?", (F"%{problem_id}%",))
+        row = cursor.fetchone()
+        return row
 
     def testDB(self):
         cursor = self.cursor
         self.create_tables()
 
-        cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES (69, 'B', 'Bets', 'PROGRAMMING', 1000.0, 1200)")
-        cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES (55, 'B', 'Bets', 'PROGRAMMING', 1000.0, 900)")
-        cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES (32, 'A', 'Bets', 'PROGRAMMING', 1000.0, 800)")
+        # cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES ('69B', 'B', 'Bets', 'PROGRAMMING', 1000.0, 1200)")
+        # cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES ('55B', 'B', 'Bets', 'PROGRAMMING', 1000.0, 900)")
+        # cursor.execute("INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES ('32A', 'A', 'Bets', 'PROGRAMMING', 1000.0, 800)")
 
 
-        for pair in tagsId.items():
-            cursor.execute(f"INSERT INTO Tags (tagId, tagName) VALUES ({pair[0]}, '{pair[1]}' )")
+        # for pair in tagsId.items():
+        #     cursor.execute(f"INSERT INTO Tags (tagId, tagName) VALUES ({pair[0]}, '{pair[1]}' )")
 
-        cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({1}, {69})")
-        cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({1}, {55})")
-        cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({2}, {32})")
+        # cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({1}, {69})")
+        # cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({1}, {55})")
+        # cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({2}, {32})")
 
-        print(self.get_problems_by_tag(1))
+        print(self.get_problem_by_id("53C"))
 
-        self.addStatisticToProblem(69, 10000)
-        print(self.get_problems_by_tag(1))
+        # self.addStatisticToProblem(69, 10000)
+        # print(self.get_problems_by_tag(1))
 
         
 
@@ -76,14 +88,22 @@ class ProblemsDataBase():
     def addProblem(self, problemId, problem_index, name, problem_type, points, rating, tags: list):
         cursor = self.cursor
 
-        cursor.execute(f"INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES ({problemId}, '{problem_index}', '{name}', '{problem_type}', {points}, {rating})")
+        cursor.execute(f"INSERT INTO Problems (problemId, problem_index, name, type, points, rating) VALUES ('{problemId}', '{problem_index}', '{name}', '{problem_type}', {points}, {rating})")
         for tag in tags:
             cursor.execute(F"INSERT INTO Problems_Tags (tagId, problemId) VALUES ({idTags.get(tag)}, {problemId})")
 
-        
     
     def addStatisticToProblem(self, tag, solved_count):
         cursor = self.cursor
         cursor.execute("UPDATE Problems SET statistic =? WHERE problemId =?", (solved_count, tag))
         self.connect.commit()
 
+    def printAll(self):
+        cursor = self.cursor
+
+        cursor.execute("SELECT * FROM Problems")
+        print(cursor.fetchall())
+
+        # TODO: напиши запросы по teg, problemId, points, rating
+
+db = ProblemsDataBase()
--- parse_problems.py
+++ parse_problems.py
@@ -1,13 +1,31 @@
 import requests
 
 from tag_consts import IMPLEMENTATION
+from db import db
 
 PROBLEMS_REQUEST = "https://codeforces.com/api/problemset.problems?tags="
 
 problems_set = requests.get(f"{PROBLEMS_REQUEST}{IMPLEMENTATION}")
 
+# TODO: ужасный преужасный костыль. Нужен чтобы упростить структуру бд, но мне нужно будет сделать поиск контеста по части сроки (% вроде бы)... 
+def createId(id, index):
+    return f"{id}{index}"
+
 # для записи в таблицу нужно к id контеста добавлять код индекса задачи.
 
-print(problems_set.content)
-# for problem in problems_set.content:
-#     print(problem) 
+# print(createId(345, "A"))
+for problem in problems_set.json()["result"]["problems"]:
+    id = str(problem.get("contestId"))+problem.get("index")
+    index = problem.get("index")
+    name = problem.get("name")
+    type = problem.get("type")
+    points = problem.get("points")
+    rating = problem.get("rating")
+    tags = problem.get("tags")
+    try:
+        db.addProblem(id, index, name, type, points, rating, tags)
+    except:
+        pass
+
+db.printAll()
+db.testDB()
